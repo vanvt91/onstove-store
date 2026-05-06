7f1cbe3ef80ba5a96b544d3207c283dbf219ab49
vanvt91 <elvis.van91@gmail.com>
2026-05-06 15:47:17 +0700

    up

--- src/data/login.data.ts
+++
@@ -1,7 +0,0 @@
-export const loginData = {
-  validEmail: "qa.user@mailinator.com",
-  password: "Test@1234",
-  invalidPassword: "wrongpassword",
-  userNotExist: "notexistuser@mailinator.com",
-  invalidEmail: "test@invalid",
-};
